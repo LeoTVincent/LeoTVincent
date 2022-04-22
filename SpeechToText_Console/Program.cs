97b9f86ef89e0cea1d73884990dde9b040a85623
LeoTVincent <leopmir@gmail.com>
2022-04-22 22:41:11 +0530

    Text-toSpeech and Speech-To-Text sample console project

---
+++ SpeechToText_Console/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+
+namespace SpeechToText_Console
+{
+    class Program
+    {
+        static string YourSubscriptionKey = "dc53284da3e24aab8925d81c3e4c414f";
+        static string YourServiceRegion = "westus2";
+
+        static void OutputSpeechRecognitionResult(SpeechRecognitionResult speechRecognitionResult)
+        {
+            switch (speechRecognitionResult.Reason)
+            {
+                case ResultReason.RecognizedSpeech:
+                    Console.WriteLine($"RECOGNIZED: Text={speechRecognitionResult.Text}");
+                    break;
+                case ResultReason.NoMatch:
+                    Console.WriteLine($"NOMATCH: Speech could not be recognized.");
+                    break;
+                case ResultReason.Canceled:
+                    var cancellation = CancellationDetails.FromResult(speechRecognitionResult);
+                    Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
+
+                    if (cancellation.Reason == CancellationReason.Error)
+                    {
+                        Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
+                        Console.WriteLine($"CANCELED: ErrorDetails={cancellation.ErrorDetails}");
+                        Console.WriteLine($"CANCELED: Did you set the speech resource key and region values?");
+                    }
+                    break;
+            }
+        }
+
+        async static Task Main(string[] args)
+        {
+            var speechConfig = SpeechConfig.FromSubscription(YourSubscriptionKey, YourServiceRegion);
+            speechConfig.SpeechRecognitionLanguage = "en-US";
+
+            //To recognize speech from an audio file, use `FromWavFileInput` instead of `FromDefaultMicrophoneInput`:
+            //using var audioConfig = AudioConfig.FromWavFileInput("YourAudioFile.wav");
+            using var audioConfig = AudioConfig.FromDefaultMicrophoneInput();
+            using var speechRecognizer = new SpeechRecognizer(speechConfig, audioConfig);
+
+            Console.WriteLine("Speak into your microphone.");
+            var speechRecognitionResult = await speechRecognizer.RecognizeOnceAsync();
+            OutputSpeechRecognitionResult(speechRecognitionResult);
+        }
+    }
+}
---
+++ TextToSpeech_console/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+using System.Speech.Synthesis;
+
+namespace TextToSpeech_console
+{
+    class Program
+    {
+        static string YourSubscriptionKey = "";
+        static string YourServiceRegion = "westus2";
+
+        static void OutputSpeechSynthesisResult(SpeechSynthesisResult speechSynthesisResult, string text)
+        {
+            switch (speechSynthesisResult.Reason)
+            {
+                case ResultReason.SynthesizingAudioCompleted:
+                    Console.WriteLine($"Speech synthesized for text: [{text}]");
+                    break;
+                case ResultReason.Canceled:
+                    var cancellation = SpeechSynthesisCancellationDetails.FromResult(speechSynthesisResult);
+                    Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
+
+                    if (cancellation.Reason == CancellationReason.Error)
+                    {
+                        Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
+                        Console.WriteLine($"CANCELED: ErrorDetails=[{cancellation.ErrorDetails}]");
+                        Console.WriteLine($"CANCELED: Did you set the speech resource key and region values?");
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        private static void Speak(string textToSpeech, bool wait = false)
+        {
+            // Command to execute PS  
+            Execute($@"Add-Type -AssemblyName System.speech;  
+            $speak = New-Object System.Speech.Synthesis.SpeechSynthesizer;                           
+            $speak.Speak(""{textToSpeech}"");"); // Embedd text  
+
+            void Execute(string command)
+            {
+                // create a temp file with .ps1 extension  
+                var cFile = System.IO.Path.GetTempPath() + Guid.NewGuid() + ".ps1";
+
+                //Write the .ps1  
+                using var tw = new System.IO.StreamWriter(cFile, false, Encoding.UTF8);
+                tw.Write(command);
+
+                // Setup the PS  
+                var start =
+                    new System.Diagnostics.ProcessStartInfo()
+                    {
+                        FileName = "C:\\windows\\system32\\windowspowershell\\v1.0\\powershell.exe",  // CHUPA MICROSOFT 02-10-2019 23:45                    
+                        LoadUserProfile = false,
+                        UseShellExecute = false,
+                        CreateNoWindow = true,
+                        Arguments = $"-executionpolicy bypass -File {cFile}",
+                        WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden
+                    };
+
+                //Init the Process  
+                var p = System.Diagnostics.Process.Start(start);
+                // The wait may not work! :(  
+                if (wait) p.WaitForExit();
+            }
+        }
+        async static Task Main(string[] args)
+        {
+            //Speak("Hello Leo"); //this is with .NET Framework System.Speech implementation
+
+            //this is with .Net core System.Speech implementation
+            System.Speech.Synthesis.SpeechSynthesizer speech = new System.Speech.Synthesis.SpeechSynthesizer();
+            speech.SpeakAsync("Leo Thiraviyam");
+            System.Speech.Recognition.SpeechRecognizer speechRecognizer = new System.Speech.Recognition.SpeechRecognizer();
+            speechRecognizer.EmulateRecognize("");
+
+            //thi is with Azure Speech cognitive service implementation
+            //var speechConfig = SpeechConfig.FromSubscription(YourSubscriptionKey, YourServiceRegion);
+
+            //// The language of the voice that speaks.
+            //speechConfig.SpeechSynthesisVoiceName = "en-US-JennyNeural";
+
+            //using (var speechSynthesizer = new SpeechSynthesizer(speechConfig))
+            //{
+            //    // Get text from the console and synthesize to the default speaker.
+            //    Console.WriteLine("Enter some text that you want to speak >");
+            //    string text = Console.ReadLine();
+
+            //    var speechSynthesisResult = await speechSynthesizer.SpeakTextAsync(text);
+            //    OutputSpeechSynthesisResult(speechSynthesisResult, text);
+            //}
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
